f235b030eac39910094097203bd5b7070cf3e16a
kmarilleau <kevin.marilleau@gmail.com>
2018-09-22 00:34:19 +0200

    Update final_script.sh

--- centos/final_script.sh
+++ centos/final_script.sh
@@ -34,13 +34,28 @@ certbot certonly --manual \
 # Ex: 
 #     Name: _acme-challenge.yourdomain.com | Type: TXT | Data: xjPMg-I6BokgUVOyIN3NJlIqbc9xGXUzyQE98dPdt1E
 
-# Modify inventory.ini
-echo "openshift_master_overwrite_named_certificates=true" >> ./inventory.ini
-echo "openshift_master_named_certificates=[{\"certfile\": \"/etc/letsencrypt/live/${DOMAIN}/fullchain.pem\", \"keyfile\": \"/etc/letsencrypt/live/${DOMAIN}/privkey.pem\", \"names\": [\"console.${DOMAIN}\"]}]" >> ./inventory.ini
-echo "openshift_hosted_router_certificate={\"certfile\": \"/etc/letsencrypt/live/${DOMAIN}.com/cert.pem\", \"keyfile\": \"/etc/letsencrypt/live/${DOMAIN}/privkey.pem\"}" >> ./inventory.ini
-# If you don't use a certificate without wildcards, you need to replace the last command by:
-# echo "openshift_master_named_certificates=[{\"certfile\": \"/etc/letsencrypt/live/console.${DOMAIN}/cert.pem\", \"keyfile\": \"/etc/letsencrypt/live/console.${DOMAIN}/privkey.pem\", \"names\": [\"console.${DOMAIN}\"]}]" >> ./inventory.ini
+## Modify inventory.ini 
+## See here for more explanation: https://docs.okd.io/latest/install_config/certificate_customization.html
+cat <<EOT >> inventory.ini
 
+# Declare usage of Custom Certificate
+openshift_master_overwrite_named_certificates=true
+
+# Configure Custom Certificates for the Web Console or CLI
+openshift_master_cluster_hostname=console-internal.${DOMAIN}
+openshift_master_cluster_public_hostname=console.${DOMAIN}
+
+# Configure a Custom Master Host Certificate
+openshift_master_named_certificates=[{"certfile": "/etc/letsencrypt/live/${DOMAIN}/cert.pem", "keyfile": "/etc/letsencrypt/live/${DOMAIN}/privkey.pem", "names": ["console.${DOMAIN}"]}]
+
+# Configure a Custom Wildcard Certificate for the Default Router
+openshift_hosted_router_certificate={"certfile": "/etc/letsencrypt/live/${DOMAIN}/cert.pem", "keyfile": "/etc/letsencrypt/live/${DOMAIN}/privkey.pem", "cafile": "/etc/letsencrypt/live/${DOMAIN}/chain.pem"}
+
+# Configure a Custom Certificate for the Image Registry
+openshift_hosted_registry_routehost=registry.apps.${DOMAIN}
+openshift_hosted_registry_routecertificates={"certfile": "/etc/letsencrypt/live/${DOMAIN}/cert.pem", "keyfile": "/etc/letsencrypt/live/${DOMAIN}/privkey.pem", "cafile": "/etc/letsencrypt/live/${DOMAIN}/chain.pem"}
+openshift_hosted_registry_routetermination=reencrypt
+EOT
 
 # Add Cron Task to renew certificate
 echo "@monthly  certbot renew --pre-hook=\"oc scale --replicas=0 dc router\" --post-hook=\"oc scale --replicas=1 dc router\"" > certbotcron
@@ -71,5 +86,6 @@ gpasswd -a $USERNAME docker
 sed -i 's/#PermitRootLogin yes.*/PermitRootLogin no/' /etc/ssh/sshd_config
 echo "AllowUsers ${USERNAME}" >> /etc/ssh/sshd_config
 systemctl restart sshd
+
 
 reboot
