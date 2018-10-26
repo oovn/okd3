8f1570aa576efc331a87b8620fb12060d8490e31
kmarilleau <kevin.marilleau@gmail.com>
2018-10-26 18:31:10 +0200

    Update final_script.sh

--- centos/final_script.sh
+++ centos/final_script.sh
@@ -25,7 +25,7 @@ certbot certonly --manual \
                  --server https://acme-v02.api.letsencrypt.org/directory \
                  --agree-tos \
                  -d $DOMAIN \
-                 -d *.$DOMAIN \
+                 -d console.$DOMAIN \
                  -d *.apps.$DOMAIN \
 
 ## Add Entries on your Host DNS Zone Editor
